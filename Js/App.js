528b7b15e19ce69f98ba926d934761c71ce64e6a
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-13 18:24:37 +0530

    Calculating the difference b/w end date and current date

--- Js/App.js
+++ Js/App.js
@@ -1,16 +1,12 @@
 const endDate = "15 December 2022 8:00 PM";
 document.getElementById("end-date").innerText = endDate; // Setting date dynamically
 
-// Javascript ka date object kisi bhi date ka ek time stamp bna krr deta hai  jo 1 January 1970 se avv tkk ka
-// milliseconds hote hai to humko ussi concept ka use karte hue ye countdown banana hai
-
 function clock(){
     const end=new Date(endDate)
-    // Basically yha humne kiya ye kii humne end date ke milliseconds bna liye  
-    const now=new Date()       
-    //Ye ho gyi humari avv kii Date
-    console.log(end);
-    console.log(now);            
+    const now=new Date()                   
+    const diff=end-now;
+    console.log(diff);     // This will shows you the difference b/w end date and current date
+    
 }
 
 // Initial call
